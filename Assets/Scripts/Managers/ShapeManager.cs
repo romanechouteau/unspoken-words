c260a2fe05965302ba2d85c4fa0ba8481f652928
Alban <alban.bleicher@edu.gobelins.fr>
2022-05-24 13:34:31 +0200

    :sparkles: randomize shape position on faces

--- Assets/Scripts/Managers/ShapeManager.cs
+++ Assets/Scripts/Managers/ShapeManager.cs
@@ -12,12 +12,21 @@ public class ShapeManager : Singleton<ShapeManager>
   private List<Shape>[] shapes = new List<Shape>[3];
   private GameObject[] faces = new GameObject[3];
 
+  private Vector3[][] bounds = new Vector3[3][];
+
   void Start()
   {
     for (int i = 0; i < 3; i++)
     {
       shapes[i] = new List<Shape>();
+      // get each faces
       faces[i] = i == 0 ? faceA : i == 1 ? faceB : faceC;
+      bounds[i] = new Vector3[4];
+      // get each bounds for each faces
+      for (int j = 0; j < 4; j++)
+      {
+        bounds[i][j] = faces[i].transform.GetChild(j).localPosition;
+      }
     }
 
   }
@@ -45,11 +54,53 @@ public class ShapeManager : Singleton<ShapeManager>
     UpdateIntensities(proximities, faceIndex);
 
   }
+  Vector3 GetShapePosition(Vector3[] faceBounds)
+  {
+    float margin = 0.002f;
+    // GET X VALUE
+    // Get random x between minX and maxY range
+    float minX = faceBounds[2].x - margin;
+    float maxX = faceBounds[3].x + margin;
+    float x = Random.Range(minX, maxX);
+
+    // GET Z VALUE
+    // Define minY and maxY values
+    float minZ = faceBounds[2].z + margin;
+    float maxZ = faceBounds[0].z - margin;
+
+    // Get line equations
+    // Get slopes
+    float leftLineSlope = (faceBounds[0].z - faceBounds[2].z) / (faceBounds[0].x - faceBounds[2].x);
+    float rightLineSlope = (faceBounds[1].z - faceBounds[3].z) / (faceBounds[1].x - faceBounds[3].x);
+    // Get intercepts (ordonnée à l'origine)
+    float leftLineIntercept = faceBounds[0].z - leftLineSlope * faceBounds[0].x;
+    float rightLineIntercept = faceBounds[1].z - leftLineSlope * faceBounds[1].x;
+
+    // Project x on both line (with y=mx+b)
+    float leftProjection = leftLineSlope * x + leftLineIntercept;
+    float rightProjection = rightLineSlope * x + rightLineIntercept;
+    // Keep min value to determin Y value, then clamp between min and max
+    float maxZShape = Mathf.Clamp(Mathf.Min(leftProjection, rightProjection), minZ, maxZ);
+    float z = Random.Range(minZ, maxZShape);
+
+    // GET Y VALUE
+    // Get line equation
+    // Get slope
+    float depthLineSlope = (faceBounds[0].y - faceBounds[2].y) / (faceBounds[0].z - faceBounds[2].z);
+    // Get intercept (ordonnée à l'origine)
+    float depthLineIntercept = faceBounds[0].y - depthLineSlope * faceBounds[0].z;
+
+    float y = (depthLineSlope * z + depthLineIntercept) + 0.0008f;
+
+    return new Vector3(x, y, z);
+  }
 
   void AddShape(int faceIndex)
   {
     GameObject face = faces[faceIndex];
     GameObject newShape = Instantiate(shapePrefab, face.transform);
+    Vector3 pos = GetShapePosition(bounds[faceIndex]);
+    newShape.transform.localPosition = pos;
     Shape shape = new Shape(0f, 0f, newShape);
     shapes[faceIndex].Add(shape);
   }
